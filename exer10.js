042b74c60844ac33ce3c503bff1395ed5c097d42
phzkkjk <pedro.silva.lemes@escola.pr.gov.br>
2025-09-25 09:39:58 -0300

    Add files via upload

--- exer10.js
+++ exer10.js
@@ -1,28 +1,28 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout
 });
 
-rl.question("Digite seu peso (kg): ", (peso) => {
-  rl.question("Digite sua altura (m): ", (altura) => {
-    peso = parseFloat(peso);
-    altura = parseFloat(altura);
+rl.question("Digite seu peso (kg):",(peso) => {
+    rl.question("Digite sua altura (M):", (altura) => {
+        peso = parseFloat(peso);
+        altura = parseFloat(altura);
 
-    let imc = peso / (altura * altura);
-    console.log(`Seu IMC é: ${imc.toFixed(2)}`);
+        let imc = peso / (altura * altura);
+            console.log(`seu IMC é: ${imc.toFixed(2)}`);
 
-    if (imc < 18.5) {
-      console.log("Classificação: Abaixo do peso");
-    } else if (imc < 24.9) {
-      console.log("Classificação: Peso normal");
-    } else if (imc < 29.9) {
-      console.log("Classificação: Sobrepeso");
-    } else {
-      console.log("Classificação: Obesidade");
-    }
+        if (imc <18.5) {
+            console.log("Classificação: abaixo do peso");
+        } else if (imc < 24.9){
+            console.log("Classificação: peso normal");
+        } else if (imc < 29.9) {
+            console.log("Classificação: sobrepeso");
+        } else {
+            console.log("Classificação: obesidade");
+        }
 
-    rl.close();
-  });
+        rl.close();
+    });
 });
